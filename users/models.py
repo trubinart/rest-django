a01451c531372d2660f4396b9cae1172d0ac82dc
truart <trubinart@gmail.com>
2021-02-28 16:04:24 +0300

    try to create database

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from uuid import uuid4
+from mimesis import Person
+
+# Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64, unique=True)
+    age = models.PositiveIntegerField()
+    password = models.CharField(max_length=50)
+
+
+# person = Person('en')
+# for i in range (0,10):
+#     user = User(first_name=person.first_name(), last_name=person.last_name(),
+#                 email=person.email(), age=person.age(), password=person.password())
+#     user.save()
